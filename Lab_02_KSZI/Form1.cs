d36ffc8b6a4551e3aefdf653362e65a2c711309c
n.skryabin <n.skryabin@student.csn.khai.edu>
2018-12-05 14:45:07 +0200

    Add project files.

---
+++ Lab_02_KSZI/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_02_KSZI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var usbDeviceList = USBManager.GetListUsbDevicesId();
+
+            foreach (var item in usbDeviceList)
+            {
+                comboBox1.Items.Add(item);
+            }
+
+            foreach (var item in usbDeviceList)
+            {
+                comboBox2.Items.Add(item);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var folderPath = String.Empty;
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox1.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var folderPath = String.Empty;
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != String.Empty && comboBox1.GetItemText(comboBox1.SelectedItem) != String.Empty)
+            {
+                USBManager.Lock(textBox1.Text);
+                File.AppendAllText("config.txt", $"{textBox1.Text}-{comboBox1.GetItemText(comboBox1.SelectedItem)}-");
+
+                MessageBox.Show(
+                        "Папка успешно заблокирована",
+                        "Блокировка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                return;
+            }
+
+            MessageBox.Show(
+                        "Не выбрана папка либо не выбрано USB-устройство",
+                        "Блокировка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != String.Empty && comboBox2.GetItemText(comboBox2.SelectedItem) != String.Empty)
+            {
+                var listConfig = File.ReadAllText(@"config.txt").Replace("\n", " ");
+
+                if (listConfig.Contains(textBox2.Text+"-"+comboBox2.GetItemText(comboBox2.SelectedItem)))
+                {
+                    USBManager.Unlock(textBox2.Text);
+
+                    MessageBox.Show(
+                        "Папка успешно разблокирована",
+                        "Разблокировка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    return;
+                }
+
+                MessageBox.Show(
+                        "Неправильно выбрана папка либо USB-устройство",
+                        "Разблокировка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ Lab_02_KSZI/USBManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_KSZI
+{
+    public static class USBManager
+    {
+        public static List<string> GetListUsbDevicesId()
+        {
+            var usbDevices = GetUSBDevices().Select(x=>x.DeviceID).ToList();
+
+            return usbDevices;
+            //foreach (var usbDevice in usbDevices)
+            //{
+            //    Console.WriteLine("Device ID: {0}, PNP Device ID: {1}, Description: {2}",
+            //        usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+            //}
+
+            //Console.Read();
+        }
+
+        public static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+
+        public static void Lock(string folderPath)
+        {
+            //var folderPath = @"C:\Users\Nick\Desktop\testFolder";
+            string adminUserName = Environment.UserName;// getting your adminUserName
+            DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+            FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+
+            ds.AddAccessRule(fsa);
+            Directory.SetAccessControl(folderPath, ds);
+        }
+
+        public static void Unlock(string folderPath)
+        {
+            //var folderPath = @"C:\Users\Nick\Desktop\testFolder";
+            string adminUserName = Environment.UserName;// getting your adminUserName
+            DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+            FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+
+            ds.RemoveAccessRule(fsa);
+            Directory.SetAccessControl(folderPath, ds);
+        }        
+    }
+
+    public class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
